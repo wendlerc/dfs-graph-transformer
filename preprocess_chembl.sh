1ae2f8519ebcaf7db73ea8103e38eca8702929e0
Chris Wendler <chris.wendler@inf.ethz.ch>
2021-07-30 17:50:29 +0200

    localc hanges
    Former-commit-id: 34c6511908298ac419918f244d022b47c05802f5

--- preprocess_chembl.sh
+++ preprocess_chembl.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-for i in {0..64}
+for i in {0..63}
 do
 if [ $(((($i+1)) % 12)) -eq 0 ]
 then 
-    python scripts/chembl/compute_dfs_codes.py with nr=$i -F results/chembl
+    python scripts/chembl/compute_dfs_codes.py with nr=$i time_limit=600 -F results/chembl &
 else
-    python scripts/chembl/compute_dfs_codes.py with nr=$i -F results/chembl &
+    python scripts/chembl/compute_dfs_codes.py with nr=$i time_limit=600 -F results/chembl & 
 fi
+sleep 1;
 done
