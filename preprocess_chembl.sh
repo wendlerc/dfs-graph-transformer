646c9cc0e532753eb17b0da7493b64bf80a62bd1
Chris Wendler <chris.wendler@inf.ethz.ch>
2021-08-25 16:46:26 +0200

    random scaffold splits
    Former-commit-id: e349b695c91b0a0e2e9fb4ad7503fc5246a8c8de

--- preprocess_chembl.sh
+++ preprocess_chembl.sh
@@ -6,11 +6,6 @@ TIME_LIMIT=$3
 
 for i in {0..63}
 do
-if [ $(((($i+1)) % $N_CORES)) -eq 0 ]
-then 
-    python scripts/chembl/compute_dfs_codes.py with use_Hs=False nr=$i time_limit=$TIME_LIMIT max_nodes=$MAX_NODES -F $DIR
-else
-    python scripts/chembl/compute_dfs_codes.py with use_Hs=False nr=$i time_limit=$TIME_LIMIT max_nodes=$MAX_NODES -F $DIR &
-fi
-sleep 1;
+    python scripts/chembl/compute_dfs_codes.py with use_Hs=True nr=$i time_limit=$TIME_LIMIT max_nodes=$MAX_NODES -F $DIR &
+    sleep 1 
 done
