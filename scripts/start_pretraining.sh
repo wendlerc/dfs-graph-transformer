f7f5249a5c01a5e9fa3239b2d1ed70a61b8d8575
Chris Wendler <chris.wendler@inf.ethz.ch>
2021-09-20 16:37:09 +0200

    pretraining start script for cluster
    Former-commit-id: 4c78d1286e541a9341960b667a6eb711fb8d6f5d

--- scripts/start_pretraining.sh
+++ scripts/start_pretraining.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+MEMORY=40960
+GPU=GeForceRTX2080Ti
+
 for size in 10K 100K 1M
 do 
-    bsub -o bert$size.log -W 48:00 -R "rusage[mem=40960, ngpus_excl_p=1]" -R "select[gpu_model0==GeForceRTX2080Ti]" python exp/pretrain/selfattn/pubchem.py --yaml config/selfattn/bert$size.yaml --name bert-$size-euler
+    bsub -o bert$size.log -W 48:00 -R "rusage[mem=$MEMORY, ngpus_excl_p=1]" -R "select[gpu_model0==$GPU]" python exp/pretrain/selfattn/pubchem.py --yaml config/selfattn/bert$size.yaml --name bert-$size-euler
 done
 
 for size in 10K 100K 1M 10M
 do
-    bsub -o rnd2min$size.log -W 48:00 -R "rusage[mem=40960, ngpus_excl_p=1]" -R "select[gpu_model0==GeForceRTX2080Ti]" python exp/pretrain/selfattn/pubchem.py --yaml config/selfattn/rnd2min$size.yaml --name rnd2min-$size-euler
+    bsub -o rnd2min$size.log -W 48:00 -R "rusage[mem=$MEMORY, ngpus_excl_p=1]" -R "select[gpu_model0==$GPU]" python exp/pretrain/selfattn/pubchem.py --yaml config/selfattn/rnd2min$size.yaml --name rnd2min-$size-euler
 done
