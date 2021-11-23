c6ca9597fadb447c586183127462783fec5ca44f
Chris Wendler <chris.wendler@inf.ethz.ch>
2021-11-23 14:32:00 +0100

    install helper
    Former-commit-id: ee16182654e1d504bf0fbb666278cb22a306a263

---
+++ install_additional.sh
@@ -0,0 +1,7 @@
+CUDA=cu110
+pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.9.0+$CUDA.html --upgrade
+pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.9.0+$CUDA.html --upgrade
+pip install torch-geometric --upgrade
+pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-1.9.0+$CUDA.html --upgrade
+pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-1.9.0+$CUDA.html --upgrade
+pip install dgl-cu102 -f https://data.dgl.ai/wheels/repo.html --upgrade
