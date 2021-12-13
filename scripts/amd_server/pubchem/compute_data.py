041f6e1987be683170072c11f1bb66820697e6ca
Chris Wendler <chris.wendler@inf.ethz.ch>
2021-12-13 11:06:56 +0100

    make max nodes and max edges arguments effective
    Former-commit-id: ddb1b03ea2378c041fca323d38a590474044861f

--- scripts/amd_server/pubchem/compute_data.py
+++ scripts/amd_server/pubchem/compute_data.py
@@ -39,15 +39,16 @@ def main(path, n_splits, nr, total, max_nodes, max_edges, time_limit, log_level,
                 codes = pickle.load(f)
             d_dict = {}
             for smiles, code in tqdm.tqdm(codes.items()):
-                d = smiles2graph(smiles, useHs=use_Hs, addLoops=add_loops)
-                data = {}
-                data['x'] = d.x.detach().cpu().numpy().tolist()
-                data['z'] = d.z.detach().cpu().numpy().tolist()
-                data['edge_attr'] = d.edge_attr.detach().cpu().numpy().tolist()
-                data['edge_index'] = d.edge_index.detach().cpu().numpy().tolist()
-                data['atom_features'] = d.atom_features.detach().cpu().numpy().tolist()
-                data['bond_features'] = d.bond_features.detach().cpu().numpy().tolist()
-                d_dict[smiles] = data
+                if len(code["min_dfs_code"]) <= max_edges and len(code["dfs_index"]) <= max_nodes:
+                    d = smiles2graph(smiles, useHs=use_Hs, addLoops=add_loops)
+                    data = {}
+                    data['x'] = d.x.detach().cpu().numpy().tolist()
+                    data['z'] = d.z.detach().cpu().numpy().tolist()
+                    data['edge_attr'] = d.edge_attr.detach().cpu().numpy().tolist()
+                    data['edge_index'] = d.edge_index.detach().cpu().numpy().tolist()
+                    data['atom_features'] = d.atom_features.detach().cpu().numpy().tolist()
+                    data['bond_features'] = d.bond_features.detach().cpu().numpy().tolist()
+                    d_dict[smiles] = data
             with open(path+"/%d/data_split%d.pkl"%(i+1, didx), 'wb') as f:
                 pickle.dump(d_dict, f)
             
