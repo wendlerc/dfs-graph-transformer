08d669f087ad9cf3a0f79c2169925ea95c3a4f92
Chris Wendler <wendlerc@ethz.ch>
2022-01-13 10:46:43 +0100

    better eval script
    Former-commit-id: 55d7080918dadd6ae71ace50bf868939b10e5a1a

--- scripts/run_eval.py
+++ scripts/run_eval.py
@@ -9,24 +9,30 @@ Created on Wed Nov 24 15:27:33 2021
 import wandb 
 import subprocess
 import argparse
+import shlex
 
+print("starting evaluation script...")
 parser = argparse.ArgumentParser()
 parser.add_argument('bashcommand', type=str, help="bash \"bashcommand <pretrainedmodel>; will be run")
+parser.add_argument('--pretrained_project', type=str, default="pubchem_newloader")
 args = parser.parse_args()
 
+print("logging into wandb...")
 try:
     wandb.login(key="5c53eb61039d99e4467ef1fccd1d035bb84c1c21")
 except:
     print('already logged in')
 
+print("running experiments...")
 api = wandb.Api()
-for run in api.runs("dfstransformer/pubchem_plus_properties"):
+for run in api.runs(args.pretrained_project):
     try:
-        artifact = api.artifact('dfstransformer/pubchem_plus_properties/%s:latest'%run.name)
+        artifact = api.artifact(args.pretrained_project+"%s:latest"%run.name)
         bashCommand = args.bashcommand+' %s'
         bashCommand = bashCommand%(run.name)
         print('running: %s'%bashCommand)
-        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        print(shlex.split(bashCommand))
+        process = subprocess.Popen(shlex.split(bashCommand), stdout=subprocess.PIPE)
         output, error = process.communicate()
         print(output, error)
     except:
