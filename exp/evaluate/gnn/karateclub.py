f5c2ac0c89ad8d625d39ec2dd4d2aefa6d1917cc
Chris Wendler <chris.wendler@inf.ethz.ch>
2022-02-07 17:49:09 +0100

    karateclub experiment scripts
    Former-commit-id: bf2ccfc93387b02854f155154b6b4b90f4d85c8a

---
+++ exp/evaluate/gnn/karateclub.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  4 19:02:08 2022
+
+@author: chrisw
+"""
+
+import json
+import numpy as np
+import glob
+import wandb
+import os
+import torch.optim as optimizers
+import pandas as pd
+from copy import deepcopy
+from ml_collections import ConfigDict 
+import tqdm
+import torch.nn.functional as F
+import torch
+import torch.nn as nn
+from collections import defaultdict
+import yaml
+from sklearn.metrics import roc_auc_score
+import networkx as nx
+
+import sys
+sys.path.append("src")
+import dfs_code 
+from dfs_transformer import collate_downstream, DFSCodeSeq2SeqFC, TrainerGNN, KarateClubDataset
+import argparse
+#torch.multiprocessing.set_sharing_strategy('file_system')
+
+import torch_geometric.nn as tnn
+import networkx as nx
+from torch_geometric.loader import DataLoader
+
+def loss_pretrain(pred, y, l=nn.MSELoss()):
+    return l(pred, y)
+
+def loss(pred, y, ce=nn.CrossEntropyLoss()):
+    return ce(pred, y.squeeze())
+
+def auc(pred, y):
+    try:
+        return roc_auc_score(y.detach().cpu().numpy(), pred.detach().cpu().numpy()[:, 1])
+    except ValueError:
+        return torch.tensor(0.)
+    
+parser = argparse.ArgumentParser()
+parser.add_argument('--wandb_entity', type=str, default="dfstransformer")
+parser.add_argument('--wandb_project', type=str, default="karateclub-rep100")
+parser.add_argument('--wandb_mode', type=str, default="online")
+parser.add_argument('--wandb_dir', type=str, default="./wandb")
+parser.add_argument('--name', type=str, default=None)
+parser.add_argument('--graph_file', type=str, default="/mnt/ssd/datasets/graphs/reddit_threads/reddit_edges.json")
+parser.add_argument('--label_file', type=str, default="/mnt/ssd/datasets/graphs/reddit_threads/reddit_target.csv")
+parser.add_argument('--batch_size', type=int, default=50)
+parser.add_argument('--n_epochs', type=int, default=1)
+parser.add_argument('--learning_rate', type=float, default=0.01)
+parser.add_argument('--n_repetitions', type=int, default=100)
+parser.add_argument('--rep', type=int, default=1)
+parser.add_argument('--max_edges', type=int, default=200)
+parser.add_argument('--n_samples', type=int, default=None)
+parser.add_argument('--n_channels', type=int, default=32)
+parser.add_argument('--n_layers', type=int, default=2)
+parser.add_argument('--model', type=str, default="tnn.models.GCN")
+parser.add_argument('--readout', type=str, default="tnn.global_mean_pool")
+parser.add_argument('--num_workers', type=int, default=5)
+parser.add_argument('--pretrain_flag', action='store_true')
+parser.add_argument('--seed', type=int, default=42)
+args = parser.parse_args()
+
+config = wandb.config
+config.graph_file = args.graph_file
+config.label_file = args.label_file
+#config.graph_file = "/mnt/ssd/datasets/graphs/twitch_egos/twitch_edges.json"
+#config.label_file = "/mnt/ssd/datasets/graphs/twitch_egos/twitch_target.csv"
+config.batch_size = args.batch_size
+config.n_epochs = args.n_epochs
+config.learning_rate = args.learning_rate
+config.n_repetitions = args.n_repetitions
+config.rep = args.rep
+config.max_edges = args.max_edges
+config.n_samples = args.n_samples
+config.n_channels = args.n_channels
+config.n_layers = args.n_layers
+config.num_workers = args.num_workers
+config.model = args.model
+config.readout = args.readout
+config.seed = args.seed
+config.training = {}
+config.pretrain_flag = args.pretrain_flag
+
+run = wandb.init(mode=args.wandb_mode, project=args.wandb_project, entity=args.wandb_entity, 
+                 config=config, job_type="evaluation", name=args.name)
+
+dataset = KarateClubDataset(config.graph_file, config.label_file, max_n=config.n_samples, max_edges=config.max_edges)
+dim_input = dataset[0].x.shape[1]
+
+
+np.random.seed(config.seed)
+n = len(dataset)
+n_train = int(0.8*n)
+n_valid = 0
+n_test = n - n_train - n_valid
+perms = [np.random.permutation(n) for _ in range(config.n_repetitions)]
+scores = defaultdict(list)
+for perm in perms:
+    train_idx = torch.tensor(perm[:n_train], dtype=torch.long)
+    valid_idx = torch.tensor(perm[n_train:n_train+n_valid].tolist(), dtype=torch.long)
+    test_idx = torch.tensor(perm[n_train+n_valid:].tolist(), dtype=torch.long)
+    ce = nn.CrossEntropyLoss()
+    trainloader = DataLoader(dataset, sampler=torch.utils.data.SubsetRandomSampler(train_idx), 
+                             batch_size=config.batch_size, num_workers=config.num_workers)
+    validloader = DataLoader(dataset, sampler=torch.utils.data.SubsetRandomSampler(valid_idx), 
+                             batch_size=config.batch_size, num_workers=config.num_workers)
+    testloader = DataLoader(dataset, sampler=torch.utils.data.SubsetRandomSampler(test_idx), 
+                            batch_size=config.batch_size, num_workers=config.num_workers)
+    
+    
+    if config.pretrain_flag:
+        gnn = eval(config.model)(dim_input, config.n_channels, config.n_layers, 600)
+        premodel = tnn.Sequential('x, edge_index, batch', [
+            (gnn, 'x, edge_index -> x'),
+            (eval(config.readout), 'x, batch -> x'),
+        ])
+        
+        model = tnn.Sequential('x, edge_index, batch', [
+            (gnn, 'x, edge_index -> x'),
+            (eval(config.readout), 'x, batch -> x'),
+            (nn.Linear(600, 2),'x -> x')
+        ])
+    else:
+        gnn = eval(config.model)(dim_input, config.n_channels, config.n_layers)
+        model = tnn.Sequential('x, edge_index, batch', [
+            (gnn, 'x, edge_index -> x'),
+            (eval(config.readout), 'x, batch -> x'),
+            (nn.Linear(config.n_channels, 2),'x -> x')
+        ])
+    
+    if config.pretrain_flag:
+        pretrainer = TrainerGNN(premodel, trainloader, loss_pretrain, target='nystroem', n_epochs=config.n_epochs, lr=config.learning_rate, 
+                      es_period=n_train//config.batch_size, lr_adjustment_period=10*n_train//config.batch_size//4, wandb_run=run,
+                      clip_gradient_norm=None, lr_patience=5, es_patience=20)
+    
+    trainer = TrainerGNN(model, trainloader, loss, metrics={'auc': auc}, n_epochs=config.n_epochs, lr=config.learning_rate, 
+                  es_period=n_train//config.batch_size, lr_adjustment_period=10*n_train//config.batch_size//4, wandb_run=run,
+                  clip_gradient_norm=None, lr_patience=5, es_patience=20)
+    
+    if config.pretrain_flag:
+        pretrainer.fit()
+    trainer.fit()
+    
+    rocauc = 0.
+    with torch.no_grad():
+        model.eval()
+        pbar_valid = tqdm.tqdm(testloader)
+        for i, data in enumerate(pbar_valid):
+            data = data.cuda()
+            pred = model(data.x, data.edge_index, data.batch)
+            rocauc = (rocauc*i + auc(pred, data.y).item())/(i+1)
+
+    scores['test_roc_auc'] += [rocauc]
+    # Calculate accuracy of classification.
+    wandb.log({'test_roc_auc': rocauc})
+for key, value in scores.items():
+    wandb.log({'mean_'+key: np.mean(value),
+               'std_'+key: np.std(value)})
---
+++ src/dfs_transformer/datasets/karateclub.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  3 13:36:51 2022
+
+@author: chrisw
+"""
+
+from torch_geometric.data import Data, Dataset
+from torch_geometric.loader import DataLoader
+import torch
+import tqdm
+import networkx as nx
+from copy import deepcopy
+import numpy as np
+import torch.nn.functional as F
+import json
+import pandas as pd
+from joblib import Parallel, delayed
+
+
+def graph2labelledgraph(graph, use_dummy=False, use_degree=True):
+    graph = deepcopy(graph)
+    node_ids = np.unique(graph).tolist()
+    dummy = max(node_ids) + 1
+    
+    edge_labels = len(graph)*[0]
+    edgeindex = []
+    for e in graph:
+        edgeindex += [e]
+        edgeindex += [[e[1], e[0]]]
+        edge_labels += [0]
+    
+    if use_dummy:
+        for idx in node_ids:
+            edgeindex += [[idx, dummy], [dummy, idx]]
+            edge_labels += [1, 1]
+        node_ids += [dummy]
+    
+    edgeindex = np.asarray(edgeindex).T
+    
+    node_labels = []
+    for idx in node_ids:
+        if use_degree:
+            node_labels += [(edgeindex[0] == idx).sum()]
+        else:
+            node_labels += [1]
+    return edgeindex, node_labels, edge_labels
+
+
+class KarateClubDataset(Dataset):
+    def __init__(self, graph_file, label_file, max_n = None, max_edges=np.inf, 
+                 features=['deg', 'triangles', 'eccentricity'], n_jobs=-2):
+        super().__init__()
+        self.graph_file = graph_file
+        self.label_file = label_file
+        with open(graph_file, 'r') as f:
+            self.graph_dict = json.load(f)
+        self.label_df = pd.read_csv(label_file)
+        self.data = []
+        self.maxn = max_n
+        self.max_edges = max_edges
+        self.features = features
+        self.n_jobs = n_jobs
+        self.nystroem = np.load('/'.join(graph_file.split('/')[:-1])+'/wl_nystroem.npy')
+        self.preprocess()
+    
+    def preprocess(self):
+        use_degree = 'deg' in self.features
+        use_triangles = 'triangles' in self.features
+        use_eccentricity = 'eccentricity' in self.features
+        maxn = self.maxn
+        maxdegree = 0
+        max_edges = 0
+        edgeindex_list = []
+        vlabels_list = []
+        elabels_list = []
+        label_list = []
+        vec_list = []
+        #res_list = Parallel(n_jobs=self.n_jobs, prefer='threads')(
+        #    delayed(graph2labelledgraph)(graph, use_degree=use_degree) 
+        #    for graph in list(self.graph_dict.values()))
+        
+        for idx, graph in tqdm.tqdm(enumerate(self.graph_dict.values())):
+            res = graph2labelledgraph(graph, use_degree=use_degree) 
+            if len(res[2])//2 > self.max_edges:
+                continue
+            if maxn is not None:
+                if idx >= maxn:
+                    break
+            edgeindex_list += [res[0]]
+            vlabels_list += [res[1]]
+            elabels_list += [res[2]]
+            label_list += [self.label_df['target'][idx]]
+            vec_list += [self.nystroem[idx]]
+            maxdegree = max(maxdegree, max(res[1]))
+            max_edges = max(max_edges, len(res[2])//2)
+        self.maxdegree = maxdegree
+        self.max_edges = max_edges
+        
+        def compute_datapoint(edgeindex, vlabels, elabels, label, vec):
+            node_features = F.one_hot(torch.tensor(vlabels), num_classes=maxdegree+1).float()
+            feats = [node_features]
+            if use_triangles or use_eccentricity:
+                g = nx.Graph()
+                g.add_edges_from(edgeindex.T.tolist())
+                if use_triangles:
+                    clustering_coefs = nx.triangles(g)
+                    clustering_coefs = torch.tensor([clustering_coefs[idx] for idx in range(len(clustering_coefs))])
+                    clust_feats = F.one_hot(clustering_coefs, num_classes=self.max_edges).float()
+                    feats += [clust_feats]
+                if use_eccentricity:
+                    eccentricity = nx.eccentricity(g)
+                    eccentricity = torch.tensor([eccentricity[idx] for idx in range(len(eccentricity))], dtype=torch.long)
+                    ecc_feats = F.one_hot(eccentricity, num_classes=self.max_edges).float()
+                    feats += [ecc_feats]
+                node_features = torch.cat(feats, dim=1)
+            
+            edge_features = F.one_hot(torch.tensor(elabels), num_classes=2).float()
+            return Data(**{"edge_index": torch.tensor(edgeindex, dtype=torch.long),
+                           "edge_features": edge_features,
+                           "node_features": node_features,
+                           "node_labels": torch.tensor(vlabels, dtype=torch.long),
+                           "edge_labels": torch.tensor(elabels, dtype=torch.long),
+                           "x": node_features, 
+                           "y": torch.tensor(label, dtype=torch.long),
+                           "nystroem": torch.tensor(vec, dtype=torch.float32).unsqueeze(0),
+                           "num_nodes": len(node_features)})
+        
+        for edgeindex, vlabels, elabels, label, vec in tqdm.tqdm(zip(edgeindex_list, vlabels_list, elabels_list, label_list, vec_list)):
+            self.data += [compute_datapoint(edgeindex, vlabels, elabels, label, vec)]
+        #self.data = Parallel(n_jobs=self.n_jobs, prefer='threads')(
+        #    delayed(compute_datapoint)(edgeindex, vlabels, elabels, label)
+        #    for edgeindex, vlabels, elabels, label in zip(edgeindex_list, vlabels_list, elabels_list, label_list))
+        
+
+    def len(self):
+        return len(self.data)
+
+    def get(self, idx):
+        return self.data[idx]
